e917e52a5c2caf0e12e449a55814cb1201d288cf
Lava <69821200+LavaAdventures@users.noreply.github.com>
2022-12-20 21:02:33 -0500

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,11 @@
+echo Starting up all the bots..
+cd bots/main-bot
+echo Starting up the proxy..
+node .
+cd ..
+cd bots/sub-bot-1
+echo Starting up subbot 1..
+cd ..
+cd bots/sub-bot-2
+echo Starting up subbot 2..
+echo All bots are online! -- Running poroproxy@v2.
